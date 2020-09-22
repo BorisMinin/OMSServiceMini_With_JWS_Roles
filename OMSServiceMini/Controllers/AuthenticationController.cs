3f9e85b31ce540378ac6ceae7602ab4859db9e63
Boris Minin <boris.minin@outlook.com>
2020-09-22 20:53:47 +0300

    Add "login", "register" and "register-admin" methods in AuthenticationController

--- OMSServiceMini/Controllers/AuthenticationController.cs
+++ OMSServiceMini/Controllers/AuthenticationController.cs
@@ -12,6 +12,9 @@ using OMSServiceMini.Services.Authenticatinon;
 using OMSServiceMini.Models;
 using OMSServiceMini.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace OMSServiceMini.Controllers
 {
@@ -19,42 +22,142 @@ namespace OMSServiceMini.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        // POST api/authentication
-        [AllowAnonymous]
-        public ActionResult<string> Post(
-                AuthenticationRequest authRequest,
-                [FromServices] IJwtSigningEncodingKey signingEncodingKey)
-        {
-            // 1. Проверяем данные пользователя из запроса.
-            // ...
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
 
-            // 2. Создаем утверждения для токена.
-            var claims = new Claim[]
+        public AuthenticationController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+            this._configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
             {
-            new Claim(ClaimTypes.NameIdentifier, authRequest.Name)
-            };
+                var userRoles = await _userManager.GetRolesAsync(user);
 
-            // 3. Генерируем JWT.
-            var token = new JwtSecurityToken(
-                issuer: "OMSServiceMini",
-                audience: "OMSServiceMiniClient",
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(5),
-                signingCredentials: new SigningCredentials(
-                        signingEncodingKey.GetKey(),
-                        signingEncodingKey.SigningAlgorithm)
-            );
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
 
-            string jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
-            return jwtToken;
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
         }
 
-        [Authorize]
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> GetSomething()
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            var nameIdentifier = this.HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
-            return new string[] { nameIdentifier?.Value, "value1", "value2" };
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
+
+        [HttpPost]
+        [Route("register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+
+            if (!await _roleManager.RoleExistsAsync(UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+            if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
+                await _userManager.AddToRoleAsync(user, UserRoles.Admin);
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+        }
+        //// POST api/authentication
+        //[AllowAnonymous]
+        //public ActionResult<string> Post(
+        //        AuthenticationRequest authRequest,
+        //        [FromServices] IJwtSigningEncodingKey signingEncodingKey)
+        //{
+        //    // 1. Проверяем данные пользователя из запроса.
+        //    // ...
+
+        //    // 2. Создаем утверждения для токена.
+        //    var claims = new Claim[]
+        //    {
+        //    new Claim(ClaimTypes.NameIdentifier, authRequest.Name)
+        //    };
+
+        //    // 3. Генерируем JWT.
+        //    var token = new JwtSecurityToken(
+        //        issuer: "OMSServiceMini",
+        //        audience: "OMSServiceMiniClient",
+        //        claims: claims,
+        //        expires: DateTime.Now.AddMinutes(5),
+        //        signingCredentials: new SigningCredentials(
+        //                signingEncodingKey.GetKey(),
+        //                signingEncodingKey.SigningAlgorithm)
+        //    );
+
+        //    string jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+        //    return jwtToken;
+        //}
+
+        //[Authorize]
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> GetSomething()
+        //{
+        //    var nameIdentifier = this.HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+        //    return new string[] { nameIdentifier?.Value, "value1", "value2" };
+        //}
     }
 }
