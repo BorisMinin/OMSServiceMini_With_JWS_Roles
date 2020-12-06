6f485c7548d6f06fb6ee7f4590d38290ba162ea6
Boris Minin <boris.minin@outlook.com>
2020-12-06 13:43:06 +0300

    Add LoginModel, RegisterModel, ApplicationIdentityUser and UserRoles

---
+++ OMSServiceMini/Services/Authentication/RegistrationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMSServiceMini.Services.Authentication
+{
+    public class RegistrationModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ OMSServiceMini/Services/Authentication/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMSServiceMini.Services.Authentication
+{
+    public class UserRoles
+    {
+        public const string RoleAdmin = "Admin";
+        public const string RoleUser = "User";
+    }
+}
